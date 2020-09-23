824e164e491fbf10ce9f61ef1f664fc66dd63d6f
Sam Gilbert <sam.gilbert@ucl.ac.uk>
2020-09-23 18:00:27 +0100

    initial translation of instructions

--- WebTasks1/src/com/sam/webtasks/client/Instructions.java
+++ WebTasks1/src/com/sam/webtasks/client/Instructions.java
@@ -1,8 +1,5 @@
 package com.sam.webtasks.client;
 
-import com.sam.webtasks.basictools.Counterbalance;
-import com.sam.webtasks.iotask2.IOtask2BlockContext;
-
 public class Instructions {
 
 	public static String Get(int index) {
@@ -10,104 +7,93 @@ public class Instructions {
 		 
 		switch(index) {
 		case 0:
-			i="In this experiment you will have a simple task to do.<br><br>"
-                    + "You will see several yellow circles inside a box. "
-                    + "Inside each circle will be a number. <br><br>"
-                    + "You can move them around using your mouse. Your task is to drag them to the bottom "
-                    + "of the box in sequence. "
-                    + "Please start by dragging 1 all the way to the bottom. "
-                    + "This will make it disappear. Then drag 2 to the bottom, then 3, "
-                    + "and so on.<br><br>";
+			i="这是个很简单的实验。<br><br>"
+                    + "您会看到几个黄色的圆圈， "
+                    + "圈内会有数字。 <br><br>"
+                    + "你可以通过拖动鼠标改变他们的位置。"
+                    + "你的任务是按顺序将这些圆圈拖拽出方块区域底部，使这些圆圈消失在屏幕中。 "
+                    + "请您开始先将标有1号的圆圈拖拽到底部位置，"
+                    + "圆圈消失后， 然后顺序依次拖拽2号，3号圆圈至底部， "
+                    + "并以此类推.<br><br>";
 			break;
 		case 1:
-			i="Now you will continue the same task, but sometimes there will be something else to "
-                    + "do.<br><br>As well as dragging each circle in turn to the "
-                    + "bottom of the screen, there will sometimes be special "
-                    + "circles that you should drag in another direction (left, top, or right) instead of towards the bottom.<br><br>"
-                    + "These special circles will initially appear in a different colour "
-                    + "when they are first shown on the screen, instead of yellow. This is an "
-                    + "instruction telling you where they should go.<br><br>"
-                    + "For example, suppose that the circle with 7 in it was first shown in blue "
-                    + "when it appeared on the screen. That would be an instruction that "
-                    + "when you get to 7 in the sequence, you should drag that circle "
-                    + "to the blue side of the box (left) instead of the bottom.<br><br>"
-                    + "You will still be able to drag any "
-                    + "circle to the bottom of the box, but you should try to "
-                    + "remember to drag these special circles to the instructed "
-                    + "location instead.";
+			i="现在任务会变得更复杂一些。除了黄色的圆圈之外，还会出现一些特殊的、初始颜色不同的圆圈，"
+                    + ".<br><br>您要将他们拖拽到对应颜色的边框才行。<br><br>"
+                    + "比如说，假如7号圆圈初始颜色是蓝色的， "
+                    + "那么当轮到我们拖拽7号的时候，"
+                    + "需要将它拖拽到蓝色边框的方向，而不是正下方边框了。<br><br>"
+                    + "当然你还是可以把任何圆圈拖拽到底部边框， "
+                    + "但你需要尽量去记住将这些特殊的圆圈拖拽到对应的目标边框";
 			break;
 		case 2:
-			i="You did not drag the special circle to the instructed side of the square.";
+			i="你没有将特殊圆圈从指定的边框上移出";
 			break;
 		case 3:
-			i="Now it will get more difficult.<br><br>There will be a total of 17 circles and "
-					+ "7 of them will be special ones that should go to one of the coloured sides of the box.<br><br> "
-					+ "Don't worry if you do not remember all of them. That's fine - just try to remember as many as you can.";
+			i="现在任务将变得稍微困难一点了。<br><br>现在将会有17个圆圈出现在屏幕中， "
+					+ "并且其中有7个是要从对应颜色边框中移出的特殊圆圈。<br><br> "
+					+ "我们知道全部记住特殊圆圈对应的边框并将其移出很困难。但别担心，尽最大努力完成任务就好了。";
 			break;
-		case 4:
-			i="Now that you have had some practice with the experiment, we would like you to tell us "
-                    + "how accurately you can perform the task.<br><br>Please use the scale below to indicate what percentage of "
-                    + "the special circles you can correctly drag to the instructed side of the square, on average. 100% "
-                    + "would mean that you always get every single one correct. 0% would mean that you can never "
-                    + "get any of them correct.";
+		case 4:  
+			i="现在您已经对任务逐渐熟悉。我们想知道您认为您有多大信心将所有的特殊圆圈从对应的颜色边框移出。<br><br>"
+                    + "请用鼠标拖动下面的圆圈， "
+                    + "最左侧0%表示您认为您不能把任何一个特殊圆圈正确地移出， "
+                    + "最右侧100%表示您认为您可以将每一个特殊圆圈正确地移出 ";
 			break;
 		case 5:
-			i="Now we are going to explain a strategy that can make the task easier.<br><br>"
-                    + "When you see a special circle, you can set a reminder by immediately dragging it to a "
-                    + "different part of the box. For example, if a circle initially appeared in blue, you "
-                    + "could immediately drag it next to the blue (left) side of the box. Then, when "
-                    + "you get to that circle in the sequence its location would remind you where it is supposed "
-                    + "to go.<br><br>Please now try the task again, using this strategy to help you.";
+			i="现在我们将告诉您一个可以让任务变简单的小策略。<br><br>"
+                    + "当出现不同颜色的圆圈的时候，先立刻将特殊圆圈拖拽到对应颜色的边框边上标记，"
+                    + "比如，当一个蓝色圆圈出现的时候，"
+                    + "你可以立即将这个圆圈拖拽至蓝色边框（左边）边上。 "
+                    + "这样的标记便可在之后的任务中提醒您这个圆圈对应的边框位置。"
+                    + "<br><br>现在请尝试着使用一次这个小策略来完成一次任务。";
 			break;
 		case 6:
-			i="You need to respond correctly to at least 5 of the special circles to continue.";
+			i="你需要至少将5个特殊圆圈从对应的边框中拖拽出去才能继续。";
 			break;
 		case 7:
-			i="Now that you have had some practice with using reminders, we would like you to tell us "
-                    + "how accurately you can perform the task <b>when you use reminders to help you</b>.<br><br>Please use the scale below to indicate what percentage of "
-                    + "the special circles you can correctly drag to the instructed side of the square on average, when you use reminders. 100% "
-                    + "would mean that you always get every single one correct. 0% would mean that you can never "
-                    + "get any of them correct.";
+			i="现在您已经知道如何设置提醒来完成任务。 "
+                    + "我们想知道， <b>当可以通过设置提醒来帮助完成任务的时候，</b>您有多大信心将所有的特殊圆圈从对应的颜色边框移出。<br><br>请用鼠标拖动下面的圆圈， "
+                    + "最左侧0%表示您认为您不能把任何一个特殊圆圈正确地移出， "
+                    + "最右侧100%表示您认为您可以将每一个特殊圆圈正确地移出 ";
 			break;
-		case 8:
-			i="From now on, you will score points every time you drag one of the special circles "  
-					+ "to the correct location.<br><br>You should try to score as many points as you can.";
+		case 8:  
+			i="从现在开始，系统将为您计分。您每次将特殊圆圈拖拽到正确边框时将得分， "  
+					+ "否则不计分。<br><br>请您尽可能的去取得更高的分数。";
 			break;
-		case 9:
-			i="Sometimes when you do the task, you will have to do it without setting any reminders.<br><br>"
-                    + "When this happens, you will score 10 points for every special circle you remember.<br><br>"
-                    + "You will always be given clear instructions what you should do. In this case you will be "
-                    + "told \"This time you must do the task without setting any reminders\". "
-                    + "When this happens, "
-                    + "the computer will not let you set any reminders.";
+		case 9: 
+			i="有的时候您不能使用设置提醒的小策略，<br><br>"
+                    + "此时每将特殊圆圈从正确的边框拖拽出将得10分。<br><br>"
+                    + "在每次任务开始前，您都会被明确告知可不可以设置提醒。"
+                    + "比如现在您将被告知 \"你不得通过使用提醒完成任务\". "
+                    + "这种情况下, "
+                    + "系统将限制您设置提醒的操作。";
 			break;
 		case 10:
-			i="Other times, you will have to set reminders for all the special circles.<br><br>When "
-                + "this happens, you will also score 10 points for every special circle you remember.<br><br>"
-                + "In this case, you will be told \"This time you <b>must</b> set a reminder for every special circle\" "
+			i="然而另一些时候，你将被要求必须通过设置提醒完成任务<br><br> "
+                + "此时每将特殊圆圈从正确的边框拖拽出也将得10分。<br><br>"
+                + "而这种情况下，您将被告知 \"现在您 <b>必须</b> 对每一个特殊圆圈进行标记提醒\" "
                 + ".<br><br>"
-                + "When this happens, the computer will make sure that you always set a reminder for every "
-                + "circle and it will not let you continue if you do not.";
+                + "此时系统将强制提示您先做标记，再进行下一步任务。";
 		break;
 		case 11:
-			i="Sometimes, you will have a choice between two options when you do the task. One option will be to do the task "
-                + "without being able to set any reminders. If you choose this option, you will always score "
-                + "10 points for every special circle you remember.<br><br>The other option will be to "
-                + "do the task with reminders, but in this case each special circle will be worth "
-                + "fewer points. For example, you might be told that if you want to use reminders, "
-                + "each special circle will be worth only 5 points.<br><br>You should choose whichever "
-                + "option you think will score you the most points. <br><br>So if, for example, you "
-                + "thought you would earn more points by setting reminders and scoring 5 points for "
-                + "each special circle, you should choose this option. But if you thought you would "
-                + "score more points by just using your own memory and earning 10 points for each special "
-                + "circle, you should choose this option instead.";
+			i="然而在另一些时候，你可以选择是否通过设置提醒来完成任务。"
+                + "如果你选择不使用标记提醒，您每次将一个特殊圆圈正确移出后将得10分 "
+                + "<br><br>但当您选择使用标记提醒来帮助完成任务的时候， "
+                + "每个特殊圆圈被正确移出后的得分则将有所减少"
+                + " 比如，你可能会被告知，当选择使用设置标记提醒完成任务的时候 "
+                + "移除每个特殊圆圈将得到5分。<br><br> "
+                + "请记住，您的目标是尽量获得更多的积分。<br><br>所以，比如 "
+                + "您觉得通过设置提醒，来移除每个价值5分的特殊圆圈能获得更高得分， "
+                + "那么您应该选择使用标记提醒。 但是， "
+                + "如果您觉得不使用标记提醒，凭自己的记忆来移除每个价值10分的特殊圆圈能获得更高得分。"
+                + "那么，您应该选择不使用标记提醒。";
 		break;
 		case 12:
-			i="When you are presented with a choice like this, it is completely up to you. "
-                    + "You should do whatever you think will allow you to score the highest number of points.";
+			i="总之，当面临这样任务策略选择的时候， "
+                    + "您可以任意选择您认为可以获得更高分数的方法。";
 			break;	
 		case 13:
-			i="You have now completed the experiment.<br><br>Thank you.";
+			i="您已完成本项任务。<br><br>感谢参与！";
 			break;
 		}
 
@@ -115,43 +101,6 @@ public class Instructions {
 	}
 	
 	public static String InfoText() {
-		return ("We would like to invite you to participate in this research project. "
-                + "You should only participate if you want to; choosing not to take part "
-                + "will not disadvantage you in any way. Before you decide whether you "
-                + "want to take part, please read the following information carefully and "
-                + "discuss it with others if you wish. Ask us if there is anything that "
-                + "is not clear or you would like more information.<br><br>"
-                + "We are recruiting volunteers from the Amazon Mechanical Turk website to "
-                + "take part in an experiment aiming to improve our understanding of human "
-                + "attention and memory. You will see various objects on the screen like coloured, numbered circles, "
-                + "and you will be asked to move them with your computer mouse. Sometimes you will be asked to remember "
-                + "particular numbers and move the corresponding circle in a particular direction. You will be asked how "
-                + "confident you are in your ability to solve the task. "
-                + "The experiment "
-                + "will last approximately 45 minutes and you will receive a payment of $2 plus an additional bonus via the "
-                + "Amazon Mechanical Turk payment system. There are no anticipated risks or "
-                + "benefits associated with participation in this study.<br><br>"
-                + "It is up to you to decide whether or not to take part. If you choose "
-                + "not to participate, you won't incur any penalties or lose any "
-                + "benefits to which you might have been entitled. However, if you do "
-                + "decide to take part, you can print out this information sheet and "
-                + "you will be asked to fill out a consent form on the next page. "
-                + "Even after agreeing to take "
-                + "part, you can still withdraw at any time and without giving a reason. If you withdraw before the "
-                + "end of the experiment, we will not retain your data and it will not be analysed."
-                + "<br><br>All data will be collected and stored in accordance with the General Data Protection "
-                + "Regulations 2018. Personal information is stored separately from test results, and researchers "
-                + "on this project have no access to this data. Your personal information such as name and email "
-                + "address is held by Amazon Mechanical Turk but the researchers on this project have no acccess "
-                + "to this. Data from this experiment may be made available to the research community, for example by "
-                + "posting them on websites such as the Open Science Framework (<a href=\"http://osf.io\">http://osf.io</a>). "
-                + "It will not be possible to identify you from these data.<br><br>"
-                + "We aim to publish the results of this project in scientific journals and book chapters. Copies of the "
-                + "results can either be obtained directly from the scientific journals' websites or from us.<br><br>"
-                + "Should you wish to raise a complaint, please contact the Principal Investigator of this project, "
-                + "Dr Sam Gilbert (<a href=\"mailto:sam.gilbert@ucl.ac.uk\">sam.gilbert@ucl.ac.uk</a>). However, "
-                + "if you feel your complaint has not been handled to your satisfaction, please be aware that you can "
-                + "also contact the Chair of the UCL Research Ethics Committee (<a href=\"mailto:ethics@ucl.ac.uk\">ethics@ucl.ac.uk</a>).");
-    }
-
+		return("");
+	}
 }
